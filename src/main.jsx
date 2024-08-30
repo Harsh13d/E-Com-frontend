aa900175bc01738ccc51e09a8293053735e854f0
Dev Harsh <devharsh466@gmail.com>
2024-08-31 04:15:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { ProductContextProvider } from "./context/ProductContext.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CartContextProvider } from "./context/CartContext.jsx";
 
-export const server = "https://e-com-server-wrn5.onrender.com/";
+export const server = "https://e-com-server-wrn5.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
